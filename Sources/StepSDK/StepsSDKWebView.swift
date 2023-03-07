7aa8efe2048eb3ab2326401ef899d38afb6a06ee
Jens Wikholm <122015244+oceanleaf@users.noreply.github.com>
2023-03-07 07:59:45 +0000

    - First Commit

---
+++ Sources/StepSDK/StepsSDKWebView.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jens Wikholm on 07/03/2023.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+public class StepsSDKWebView: WKWebView {
+    
+    public init() {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.preferences.javaScriptEnabled = true
+        super.init(frame: .zero, configuration: webConfiguration)
+        self.scrollView.isScrollEnabled = false
+        self.isMultipleTouchEnabled = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    @discardableResult
+    public func load() -> WKNavigation? {
+        if let url = URL(string:"http://google.com") {
+            let req = URLRequest(url: url)
+            return super.load(req)
+        }
+        return nil
+    }
+}
---
+++ Sources/StepSDK/StepsWidget.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jens Wikholm on 07/03/2023.
+//
+
+import Foundation
+import UIKit
+
+
+public class StepsWidget: UIView {
+    
+    
+    
+}
